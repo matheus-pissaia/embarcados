294409ba235ba6bb0a3d22a37b69e162adc0da4f
Matheus Pissaia <pissaia.matheus03@gmail.com>
2024-11-10 20:26:44 -0300

    refactor: API interactions

--- irrigador.ino
+++ irrigador.ino
@@ -8,51 +8,75 @@
 #define SOIL_MOISTURE_PIN A0
 #define RELAY_PIN D3
 
+WiFiClient wifiClient;
+
 // WiFi credentials
 const char *ssid = "wifi-name";
 const char *password = "wifi-password";
-const char *API_URL = 'https://test.com'; // TODO set correct URL
+
+String BASE_URL = "http://192.168.0.167:3333/rega"; // TODO set correct URL
 
 // Create AsyncWebServer object on port 80
 AsyncWebServer server(80);
 
-// Variable to store soil moisture level
 int soilMoistureValue = 0;
-int activationLevel = 20; // TODO load activation level from server
+int activationLevel = 20;
+
+unsigned long lastPostTime = 0;
+const unsigned long postInterval = 2000; // 2 seconds
 
 void notFound(AsyncWebServerRequest *request) {
   request->send(404, "text/plain", "Not found");
 }
 
-void sendMoistureLevel(int *moistureLevel) {
+void sendMoistureLevel(int moistureLevel) {
   HTTPClient http;
+  http.begin(wifiClient, BASE_URL + "/moisture");
+  http.addHeader("Content-Type", "application/json"); // Set content type to JSON
+  
+  // Create JSON document
   JsonDocument doc;
-  String json;
+  doc["moisture"] = moistureLevel;
 
-  doc["umidade"] = moistureLevel;
+  String json;
   serializeJson(doc, json);
 
-  http.begin(API_URL + "/regas");
   http.POST(json);
-
   http.end();
 }
 
+void getActivationLevel() {
+  HTTPClient http;
+  DynamicJsonDocument doc(1024);
+
+  http.begin(wifiClient, BASE_URL + "/activation");
+  http.GET();
+
+  String payload = http.getString();
+
+  http.end();
+
+  deserializeJson(doc, payload);
+
+  if (doc["activation"])
+    activationLevel = doc["activation"].as<int>();
+}
+
 void setup() {
-  // Start serial communication
-  Serial.begin(115200);
-
-  // Prevents reconnection issues (taking too long to connect)
-  Wifi.mode(WIFI_OFF)
-
   // Set relay pin as output
   pinMode(RELAY_PIN, OUTPUT);
   digitalWrite(RELAY_PIN, HIGH);  // Ensure the relay is off initially
 
-  delay(1000)
+  // Start serial communication
+  Serial.begin(115200);
+
+  // Prevents reconnection issues (taking too long to connect)
+  WiFi.mode(WIFI_OFF);
+
+  delay(1000);
 
   // Hides the viewing of ESP as wifi hotspot
-  Wifi.mode(WIFI_STA) 
+  WiFi.mode(WIFI_STA);
 
   // Start WiFi
   WiFi.begin(ssid, password);
@@ -72,9 +96,12 @@ void setup() {
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
+  // Set activation level saved previously in the API
+  getActivationLevel();
+
   // -------- ROUTES --------
 
-  // Should be able to set activation level
+  // Should be able to set activation level from API (Use polling instead?)
   server.on("/activation-level", HTTP_POST, [](AsyncWebServerRequest *request){},
     NULL,
     [](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total) {
@@ -109,9 +136,10 @@ void setup() {
 }
 
 void loop() {
+  unsigned long currentTime = millis();
+
   soilMoistureValue = map(analogRead(SOIL_MOISTURE_PIN), 1023, 490, 0, 100);
 
-  // Automatic irrigation control logic
   if (soilMoistureValue < activationLevel) { // Adjust threshold as needed
     digitalWrite(RELAY_PIN, LOW);  // Start irrigation
   }
@@ -119,7 +147,11 @@ void loop() {
     digitalWrite(RELAY_PIN, HIGH); // Stop irrigation
   }
 
-  sendMoistureLevel(soilMoistureLevel);
+  // Send moisture value every 2 seconds
+  if (currentTime - lastPostTime >= postInterval) {
+    lastPostTime = currentTime;
+    sendMoistureLevel(soilMoistureValue);
+  }
 
-  delay(1000); // Check humidity level every 1 seconds
+  delay(500);
 }
