894ae9c9080118e168f231c5e050cde235771867
Matheus Pissaia <pissaia.matheus03@gmail.com>
2024-11-28 16:55:21 -0300

    feat: initialize frontend folder

---
+++ Frontend/.eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    root: true,
+    extends: [
+        '@ryze-blockchain/eslint-config/base/nuxt',
+        '@ryze-blockchain/eslint-config/nuxt',
+    ],
+    rules: {
+        'vue/require-default-prop': 'off',
+    },
+}
