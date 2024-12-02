e0261b8fa3d7b28505e3a08af40967f9ee414dd8
Matheus Pissaia <pissaia.matheus03@gmail.com>
2024-12-02 18:31:51 -0300

    chore: reduce icon size

--- Frontend/components/Icons/WaterDrop.vue
+++ Frontend/components/Icons/WaterDrop.vue
@@ -1,8 +1,8 @@
 <template>
     <svg
         xmlns="http://www.w3.org/2000/svg"
-        width="32"
-        height="32"
+        width="28"
+        height="28"
         viewBox="0 0 24 24"
         style="fill: currentColor"
     >
