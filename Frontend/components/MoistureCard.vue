5c4d3936c7b2ed8ae38ca20c13fbc9120c8b7db6
Matheus Pissaia <pissaia.matheus03@gmail.com>
2024-12-02 18:32:56 -0300

    refactor: main page into separated components

---
+++ Frontend/components/MoistureCard.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="col-span-2 p-16 rounded-16 bg-gray-700">
+        <div class="flex items-center gap-16">
+            <div class="p-8 rounded-8 bg-blue-700">
+                <IconsWaterDrop />
+            </div>
+
+            <div>
+                <p class="text-gray-300 text-14">
+                    Humidity
+                </p>
+                <p class="text-28 font-600 leading-1">
+                    {{ percentage }}%
+                </p>
+            </div>
+
+            <PeriodSelector v-model="selectedPeriod" class="ml-auto" />
+        </div>
+
+        <canvas ref="canvasElement" class="max-h-296 aspect-video mt-16" />
+    </div>
+</template>
+
+<script setup lang="ts">
+    import {
+        setupChart,
+        buildMoistureChart,
+        updateMoistureChart,
+        type DataPoint,
+    } from '~/composables/chart'
+    import { dayjs } from '~/helpers/dayjs'
+    import { API } from '~/services/api'
+
+    type TimePeriod = 1 | 7 | 30 // Days
+
+    const percentage = ref(0)
+    const chartDataset = ref<DataPoint[]>()
+    const selectedPeriod = ref<TimePeriod>(1)
+    const canvasElement = ref<HTMLCanvasElement>()
+
+    const filteredChartDataset = computed(() => {
+        if (!chartDataset.value)
+            return []
+
+        return filterDatasetByDate(chartDataset.value, selectedPeriod.value)
+    })
+
+    function filterDatasetByDate(dataset: DataPoint[], period: TimePeriod): DataPoint[] {
+        const timePeriodDate = dayjs().subtract(period, 'days').startOf('day')
+
+        return dataset.filter(
+            dataPoint => dayjs(dataPoint.x).isSameOrAfter(timePeriodDate),
+        )
+    }
+
+    async function updateMoisturePercentage() {
+        const data = await API.getMoisture()
+
+        if (!chartDataset.value)
+            chartDataset.value = data.map(entry => ({ x: entry.dataHora, y: entry.umidade }))
+
+        percentage.value = data.length ? data[data.length - 1].umidade : 0
+
+        setTimeout(updateMoisturePercentage, 2_000)
+    }
+
+    setupChart(
+        canvasElement,
+        filteredChartDataset,
+        buildMoistureChart,
+        updateMoistureChart,
+    )
+
+    onMounted(() => updateMoisturePercentage)
+</script>
+
