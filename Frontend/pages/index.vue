eee3be106a792c6e15da19a1976218e378ad0aa1
Matheus Pissaia <pissaia.matheus03@gmail.com>
2024-12-04 10:50:09 -0300

    chore: remove commented code

--- Frontend/pages/index.vue
+++ Frontend/pages/index.vue
@@ -3,17 +3,6 @@
         <div class="mx-auto grid grid-cols-2 max-w-2xl max-sm:px-16 gap-18">
             <MoistureCard />
 
-            <!-- <div class="flex flex-col justify-between size-160 rounded-16 bg-blue-500 p-16">
-                <IconsWaves />
-
-                <div>
-                    <p>Water usage</p>
-                    <p class="text-32 font-600">
-                        21.5L
-                    </p>
-                </div>
-            </div> -->
-
             <ActivationLevelCard />
         </div>
     </div>
